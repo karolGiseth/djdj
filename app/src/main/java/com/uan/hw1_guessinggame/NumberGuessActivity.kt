f0b4273537a163e1253b4b69a9aaef2a5bc9212f
Karol Giseth Acuña <42015229+karolGiseth@users.noreply.github.com>
2020-03-14 18:19:31 -0500

    Initial commit

---
+++ app/src/main/java/com/uan/hw1_guessinggame/NumberGuessActivity.kt
@@ -0,0 +1,12 @@
+package com.uan.hw1_guessinggame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NumberGuessActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_number_guess)
+    }
+}
